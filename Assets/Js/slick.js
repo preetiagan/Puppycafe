5e3801915e67afb9398feca1f39d3718e7a65b95
sukhjinder <sukhjinder.singh@seqops.io>
2025-08-04 13:07:30 +0530

    first commit

---
+++ Assets/Js/slick.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function(){
+ $('.slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+})
+});
+
+
